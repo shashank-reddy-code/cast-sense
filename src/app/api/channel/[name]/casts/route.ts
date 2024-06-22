3a99b9155a237b81e178365efd4c66ffaba21d97
shashank-reddy-code <shashank.am@outlook.com>
2024-06-22 08:09:12 -0700

    try removing cacche for channel top casts

--- src/app/api/channel/[name]/casts/route.ts
+++ src/app/api/channel/[name]/casts/route.ts
@@ -58,7 +58,7 @@ export async function GET(
   };
 
   const headers = new Headers();
-  headers.set("Cache-Control", "s-maxage=3600");
+  headers.set("Cache-Control", "no-store");
 
   return new NextResponse(JSON.stringify(data), {
     headers,
