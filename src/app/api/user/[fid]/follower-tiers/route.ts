6318a92762e28746e6fd1e852475fc1bc8a12cb2
Shashank Reddy <63827524+shashank-reddy-code@users.noreply.github.com>
2024-09-12 11:34:12 -0700

    deprecate power badge from follower-tiers

--- src/app/api/user/[fid]/follower-tiers/route.ts
+++ src/app/api/user/[fid]/follower-tiers/route.ts
@@ -8,28 +8,23 @@ export async function GET(
   { params }: { params: { fid: string } }
 ) {
   const fid = parseInt(params.fid);
-  const [data, powerbadgeData] = await Promise.all([
-    fetchFirstFidFromDune(3697320, fid),
-    fetchFirstFidFromDune(3696361, fid),
-  ]);
+  const data = await fetchFirstFidFromDune(3697320, fid);
 
   const headers = new Headers();
   headers.set("Cache-Control", "s-maxage=60");
-  if (!data || !powerbadgeData) {
+  if (!data) {
     return new NextResponse(null, { status: 404 });
   }
 
-  const result = await processFollowerTiers(data, powerbadgeData);
+  const result = await processFollowerTiers(data);
 
   return new NextResponse(JSON.stringify(result), { headers });
 }
 
 async function processFollowerTiers(
   data: any,
-  powerbadgeData: any
 ): Promise<FollowerTier[]> {
   const followerTiers = data;
-  const powerbadgeFollowers = powerbadgeData;
   const tierCounts: { [key: string]: number } = followerTiers.tier_name_counts;
   const tierPercentages: { [key: string]: number } =
     followerTiers.tier_name_percentages;
@@ -46,11 +41,5 @@ async function processFollowerTiers(
     return b.percentage - a.percentage;
   });
 
-  const powerbadgeFollowerTier: FollowerTier = {
-    tier: "⚡ power badge",
-    count: powerbadgeFollowers?.count || 0,
-    percentage: parseFloat(powerbadgeFollowers?.percentage || 0),
-  };
-
-  return [powerbadgeFollowerTier, ...sortedMap];
+  return [...sortedMap];
 }
